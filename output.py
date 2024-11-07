9c289bddfccb70abd8c4559f02503139261afd63
nasreen2804 <nasreenbegum.nizam@gmail.com>
2024-11-07 19:44:37 +0530

    added

---
+++ output.py
@@ -0,0 +1,17 @@
+Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============ RESTART: C:/Users/begumnb/Documents/PYTHON/Varaible.py ============
+20
+>>> 
+============ RESTART: C:/Users/begumnb/Documents/PYTHON/Varaible.py ============
+<class 'int'>
+>>> 
+============ RESTART: C:/Users/begumnb/Documents/PYTHON/Varaible.py ============
+<class 'str'>
+>>> 
+============ RESTART: C:/Users/begumnb/Documents/PYTHON/Varaible.py ============
+<class 'int'>
+>>> 
+============ RESTART: C:/Users/begumnb/Documents/PYTHON/Varaible.py ============
+30
