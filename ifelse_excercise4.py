52a69e217a907f50365a990bb16539e99d5acfeb
nasreen2804 <nasreenbegum.nizam@gmail.com>
2024-11-16 20:52:59 +0530

    phython

---
+++ ifelse_excercise4.py
@@ -0,0 +1,10 @@
+score= int (input("enter score:"))
+if(score<35):
+    print("poor student")
+elif(score>=35 and score<70):
+    print("average student")
+elif(score>=70 and score<=100):
+    print("good student")
+else:
+    print("invalid Number")
+
